1f5413cda95f8fd05d09be9f97a2800db705017e
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-04-30 09:33:40 +0530

    implemeneted the UI of login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,22 +1,76 @@
-import { Container, Paper } from "@mui/material";
+import { Button, Container, Paper, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 const Login = () => {
   const [isLogin, setIsLogin] = useState(true);
 
+  const toggleLogin = () => {
+    setIsLogin((prev) => !prev);
+  };
+
   return (
     <>
-      <Container component={"main"} maxWidth="sx">
+      <Container
+        component={"main"}
+        maxWidth="xs"
+        sx={{
+          height: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Paper
           elevation={3}
           sx={{
             padding: 4,
             display: "flex",
             flexDirection: "column",
-            alignItem: "center",
+            alignItems: "center",
           }}
         >
-          {isLogin ? <span>Login</span> : <span>Register</span>}
+          {isLogin ? (
+            <>
+              <Typography variant="h5">Login</Typography>
+              <form style={{ width: "100%", marginTop: "1rem" }}>
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  margin="normal"
+                  variant="outlined"
+                />
+                <TextField
+                  required
+                  fullWidth
+                  type="password"
+                  margin="normal"
+                  label="Password"
+                  variant="outlined"
+                />
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  sx={{ marginTop: "1rem" }}
+                  fullWidth
+                >
+                  Login
+                </Button>
+
+                <Typography textAlign={"center"} m={"1rem"}>
+                  Or
+                </Typography>
+
+                <Button fullWidth variant="text" onClick={toggleLogin}>
+                  Sign Up Instead
+                </Button>
+              </form>
+            </>
+          ) : (
+            <span>Register</span>
+          )}
         </Paper>
       </Container>
     </>
