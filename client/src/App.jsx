ace9c177e4ae2af26578b75c68945ace5e328b32
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-04-30 19:01:05 +0530

    implemented the protectRoute- if user is login then he will not be able to see the login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,36 @@
 import { lazy } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProtectRoute from "./components/auth/ProtectRoute";
 
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Group = lazy(() => import("./pages/Group"));
 const Chat = lazy(() => import("./pages/Chat"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+
+let user = true;
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/chat/:chatId" element={<Chat />} />
-          <Route path="/group" element={<Group />} />
-          <Route path="/login" element={<Login />} />
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatId" element={<Chat />} />
+            <Route path="/group" element={<Group />} />
+          </Route>
+
+          <Route
+            path="/login"
+            element={
+              <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }
+          />
+
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
