fcccf5b5e7b62c3ef484cb20c03a9c81216c21fb
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-07 13:26:05 +0530

    Implemented the layout of the admin page

---
+++ client/src/components/layout/AdminLayout.jsx
@@ -0,0 +1,147 @@
+import {
+  Close as CloseIcon,
+  Dashboard as DashboardIcon,
+  ExitToApp as ExitToAppIcon,
+  Group as GroupIcon,
+  ManageAccounts as ManageAccountsIcon,
+  Menu as MenuIcon,
+  Message as MessageIcon,
+} from "@mui/icons-material";
+import {
+  Box,
+  Drawer,
+  Grid,
+  IconButton,
+  Stack,
+  Typography,
+  styled,
+} from "@mui/material";
+import { useState } from "react";
+import { Link as LinkComponent, Navigate, useLocation } from "react-router-dom";
+import { grayColor, matBlack } from "./../../constants/color";
+
+const Link = styled(LinkComponent)`
+  text-decoration: none;
+  border-radius: 2rem;
+  padding: 1rem 2rem;
+  color: black;
+  &:hover {
+    color: rgba(0, 0, 0, 0.54);
+  }
+`;
+
+const adminTabs = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    icon: <DashboardIcon />,
+  },
+  {
+    name: "Users",
+    path: "/admin/users-management",
+    icon: <ManageAccountsIcon />,
+  },
+  {
+    name: "Chats",
+    path: "/admin/chats-management",
+    icon: <GroupIcon />,
+  },
+  {
+    name: "Messages",
+    path: "/admin/messages",
+    icon: <MessageIcon />,
+  },
+];
+
+const Sidebar = ({ w = "100%" }) => {
+  const location = useLocation();
+
+  const logoutHandler = () => {
+    console.log("logout handler");
+  };
+
+  return (
+    <Stack width={w} direction={"column"} p={"3rem"} spacing={"3rem"}>
+      <Typography>Admin</Typography>
+
+      <Stack spacing={"1rem"}>
+        {adminTabs.map((tab) => (
+          <Link
+            sx={
+              location.pathname === tab.path && {
+                bgcolor: matBlack,
+                color: "white",
+                ":hover": { color: "gray" },
+              }
+            }
+            key={tab.path}
+            to={tab.path}
+          >
+            <Stack direction={"row"} alignItems={"center"} spacing={"1rem"}>
+              {tab.icon}
+
+              <Typography fontSize={"1.2rem"}>{tab.name}</Typography>
+            </Stack>
+          </Link>
+        ))}
+
+        <Link onClick={logoutHandler}>
+          <Stack direction={"row"} alignItems={"center"} spacing={"1rem"}>
+            <ExitToAppIcon />
+
+            <Typography fontSize={"1.2rem"}>Logout</Typography>
+          </Stack>
+        </Link>
+      </Stack>
+    </Stack>
+  );
+};
+
+const isAdmin = true;
+
+const AdminLayout = ({ children }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleMobile = () => {
+    setIsMobile((prev) => !prev);
+  };
+
+  const handleClose = () => {
+    setIsMobile(false);
+  };
+
+  if (!isAdmin) return <Navigate to="/admin" />;
+
+  return (
+    <>
+      <Grid container minHeight={"100vh"}>
+        <Box
+          sx={{
+            display: { xs: "block", md: "none" },
+            position: "fixed",
+            right: "1rem",
+            top: "1rem",
+          }}
+        >
+          <IconButton onClick={handleMobile}>
+            {isMobile ? <CloseIcon /> : <MenuIcon />}
+          </IconButton>
+        </Box>
+
+        <Grid item md={4} lg={3} sx={{ display: { xs: "none", md: "block" } }}>
+          <Sidebar />
+        </Grid>
+
+        <Grid item xs={12} md={8} lg={9} sx={{ bgcolor: grayColor }}>
+          {children}
+        </Grid>
+
+        <Drawer open={isMobile} onClose={handleClose}>
+          <Sidebar w="50vw" />
+        </Drawer>
+      </Grid>
+    </>
+  );
+};
+
+export default AdminLayout;
--- client/src/pages/Admin/Dashboard.jsx
+++ client/src/pages/Admin/Dashboard.jsx
@@ -1,5 +1,11 @@
+import AdminLayout from "../../components/layout/AdminLayout";
+
 const Dashboard = () => {
-  return <>Dashboard</>;
+  return (
+    <AdminLayout>
+      <div>Dashboard</div>
+    </AdminLayout>
+  );
 };
 
 export default Dashboard;
