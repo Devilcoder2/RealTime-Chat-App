699f3e663e0e22fec508a69e8fabe5b7327eae25
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-10 00:19:21 +0530

    Implmented the chat schema

---
+++ server/models/chat.js
@@ -0,0 +1,29 @@
+import { Schema, Types, model, models } from "mongoose";
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    groupChat: {
+      type: Boolean,
+      default: false,
+    },
+    creator: {
+      type: Types.ObjectId,
+      ref: "User",
+    },
+    members: [
+      {
+        type: Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Chat = models.Chat || model("Chat", schema);
