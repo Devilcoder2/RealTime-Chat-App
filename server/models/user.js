107e101c3d0b5d4adbe92f8d1093e3b4176bda80
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-10 00:14:00 +0530

    Implemented the User schema

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+import { Schema, model, models } from "mongoose";
+
+const schema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  },
+  {
+    timestamps: true, //createdAt will be done automatically
+  }
+);
+
+export const User = models.User || model("User", schema);
