e674bfd26bf669b92975147ab34e6ada0ab322a3
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-10 00:24:14 +0530

    Implemented the message schema

---
+++ server/models/message.js
@@ -0,0 +1,34 @@
+import { Schema, Types, model, models } from "mongoose";
+
+const schema = new Schema(
+  {
+    content: String,
+
+    attachments: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    chat: {
+      type: Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Message = models.Message || model("Message", schema);
