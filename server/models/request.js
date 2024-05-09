f0e46cff86fe52e14078624b2872d9a4776a3efb
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-10 00:29:06 +0530

    Implemented the request schema

---
+++ server/models/request.js
@@ -0,0 +1,26 @@
+import { Schema, models, model, Types } from "mongoose";
+
+const schema = new Schema(
+  {
+    status: {
+      type: String,
+      default: "pending",
+      enum: ["pending", "accepted", "rejected"],
+    },
+    sender: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiver: {
+      type: Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Request = models.Request || model("Request", schema);
