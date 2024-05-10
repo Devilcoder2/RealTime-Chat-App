fdcf726193806543d0c6a55c9cb0b79965236830
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-11 04:53:28 +0530

    Implemented the login route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,7 @@
 //Create a new user and save it to the database and save in cookie
 import { User } from "../models/user.js";
 import { sendToken } from "../utils/features.js";
+import { compare } from "bcrypt";
 
 const newUser = async (req, res) => {
   const { name, username, password, bio } = req.body;
@@ -21,8 +22,16 @@ const newUser = async (req, res) => {
   sendToken(res, user, 201, "User Created");
 };
 
-const login = (req, res) => {
-  res.send("Hello World");
+const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username }).select("+password");
+  if (!user) return res.status(400).json({ message: "Invalid Username" });
+
+  const isMatch = await compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ message: "Invalid Password" });
+
+  sendToken(res, user, 201, `Welcome Back ${user.name}`);
 };
 
 export { newUser, login };
