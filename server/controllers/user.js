3355c5ea195d3650ee5a9597d6e800cc87119c73
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-11 04:40:51 +0530

    Added the jwt token in the cookie

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,6 @@
 //Create a new user and save it to the database and save in cookie
 import { User } from "../models/user.js";
+import { sendToken } from "../utils/features.js";
 
 const newUser = async (req, res) => {
   const { name, username, password, bio } = req.body;
@@ -9,7 +10,7 @@ const newUser = async (req, res) => {
     url: "adlkfads",
   };
 
-  await User.create({
+  const user = await User.create({
     name,
     username,
     bio,
@@ -17,7 +18,7 @@ const newUser = async (req, res) => {
     avatar,
   });
 
-  res.status(201).json({ message: "User created successfully" });
+  sendToken(res, user, 201, "User Created");
 };
 
 const login = (req, res) => {
--- server/utils/features.js
+++ server/utils/features.js
@@ -1,12 +1,31 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const cookieOptions = {
+  maxAge: 15 * 24 * 60 * 60 * 1000,
+  sameSite: "none",
+  httpOnly: true,
+  secure: true,
+};
 
 const connectDB = (uri) => {
   mongoose
     .connect(uri, { dbName: "Chat" })
-    .then(() => {})
+    .then((data) => {
+      console.log(`Connected to DB: ${data.connection.host}`);
+    })
     .catch((err) => {
       throw err;
     });
 };
 
-export { connectDB };
+const sendToken = (res, user, code, message) => {
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+  return res.status(code).cookie("chat-token", token, cookieOptions).json({
+    success: true,
+    message,
+  });
+};
+
+export { connectDB, sendToken };
