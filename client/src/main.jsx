e1d4e8f14c555071472f451deefe8857794b1e46
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-15 19:07:48 +0530

    Init the redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { CssBaseline } from "@mui/material";
 import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <CssBaseline />
-      <App />
-    </HelmetProvider>
+    <Provider>
+      <HelmetProvider>
+        <CssBaseline />
+        <App />
+      </HelmetProvider>
+    </Provider>
   </React.StrictMode>
 );
